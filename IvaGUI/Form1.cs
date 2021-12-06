9ed3a0e39053269630441c00306b9d0e4043c5f7
Pavel Popov <ppopov@worldbankgroup.org>
2021-12-06 15:51:30 +0200

    iva

---
+++ IvaGUI/Form1.cs
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+    End Sub
+
+    Public DataTable ReadExcel(String fileName, String fileExt) {  
+    String conn = String.Empty;  
+    DataTable dtexcel = New DataTable();  
+    If (fileExt.CompareTo(".xls") == 0)
+        conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+    Else
+        conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+    Using (OleDbConnection con = New OleDbConnection(conn)) {  
+        Try {  
+            OleDbDataAdapter oleAdpt = New OleDbDataAdapter("select * from [Sheet1$]", con); //here we read data from sheet1  
+            oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+        } catch {}  
+    }  
+    Return dtexcel;  
+}  
+End Class
---
+++ IvaGUI/ReadExcel.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Data.OleDb
+
+Public Class ReadExcel
+    Public DataTable ReadExcel(String fileName, String fileExt) {  
+    String conn = String.Empty;  
+    DataTable dtexcel = New DataTable();  
+    If (fileExt.CompareTo(".xls") == 0)
+        conn = @"provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + fileName + ";Extended Properties='Excel 8.0;HRD=Yes;IMEX=1';"; //for below excel 2007  
+    Else
+        conn = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties='Excel 12.0;HDR=NO';"; //for above excel 2007  
+    Using (OleDbConnection con = New OleDbConnection(conn)) {  
+        Try {  
+            OleDbDataAdapter oleAdpt = New OleDbDataAdapter("select * from [Sheet1$]", con); //here we read data from sheet1  
+            oleAdpt.Fill(dtexcel); //fill excel data into dataTable  
+        } catch {}  
+    }  
+    Return dtexcel;  
+}  
+
+End Class
